795bdaef78142ae1fd6532878513eed0d2339325
Yaroslav <yaroslav.boberskiy@gmail.com>
2016-01-24 01:11:06 +0200

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,49 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by YB on 23.01.2016.
+ */
+public class Demo {
+
+    public static void main(String [ ] args)
+    {
+        Map <String,Object> hm = new HashMap<String, Object>();
+
+        ArrayList ar = new ArrayList();
+        Class c = ar.getClass();
+        hm.put("size", 45);
+        initClass(c, hm);
+    }
+
+    public static <T> void initClass (Class<T> c, Map<String, Object> map) {
+        try {
+            Object obj = Class.forName(c.getName()).getConstructor().newInstance();
+            Field [] fs = obj.getClass().getDeclaredFields();
+            for (String key : map.keySet()) {
+                for (Field f : fs) {
+
+                    if (f.getName().equals(key)) {
+                        System.out.println("YES!");
+                        f.setAccessible(true);
+                        f.set(obj, map.get(key));
+                    }
+
+                }
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
